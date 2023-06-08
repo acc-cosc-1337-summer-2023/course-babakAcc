cb5c4216c95a6267bc4532a194336ff3463b978a
babakAcc <135079991+babakAcc@users.noreply.github.com>
2023-06-08 17:15:30 +0000

    practice

--- src/examples/01_data_types/01_int/main.cpp
+++ src/examples/01_data_types/01_int/main.cpp
@@ -4,10 +4,16 @@ int main()
 {
     display_number(5);
     display_number(10);
+    display_number(31);
+    display_number(53);
 
     int result = echo_number(20);
+    int result1 = echo_number(88);
+    int result2 = echo_number(77);
 
     std::cout<<"\n"<<result<<"\n";
+    std::cout<<"\n"<<result1<<"\n";
+    std::cout<<"\n"<<result2<<"\n";
     
     return 0;
 }
